b37068004d57e553edf706f3876abf28e8900efd
platonenkoolga <platonenkoolga@gmail.com>
2023-01-16 06:22:17 +0300

    carusel

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,17 @@
-const btnLocation = document.querySelector('.location_wrapper');
+const btnLocation1 = document.querySelector('.location_left');
+const btnLocation2 = document.querySelector('.header_location');
 const modal = document.querySelector('.mymodal');
 //const fr = document.querySelector('.my_map');
 const fr = document.querySelector('.map_item');
 
-btnLocation.addEventListener('click', () => {
+btnLocation1.addEventListener('click', () => {
     modal.classList.remove('hidden');
     fr.classList.remove('hidden');
+});
 
-
+btnLocation2.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    fr.classList.remove('hidden');
 });
 
 modal.addEventListener('click', (event) => {
