183b3c47836a32a5d2f338cba01da4e7d8566da6
prokopec <platonenkoolga@gmail.com>
2023-01-24 16:34:26 +0300

    modal

--- js/modules/carddescribe.js
+++ js/modules/carddescribe.js
@@ -1,15 +1,26 @@
-export  function cardmodal(){
+export function cardmodal() {
     const cardbtn = document.querySelectorAll('.card_btn');
- 
-   cardbtn.forEach((btnItem,idx) => {
-    btnItem.addEventListener('click',() => {
-        console.log(localStorage.getItem(idx));
-        //считать idx-ый элемент из localstorage
-        //убрать из класса hidden
-        //передать в innerText элемент
+    let card_modal = document.querySelector('.card_modal');
+
+    cardbtn.forEach((btnItem, idx) => {
+        btnItem.addEventListener('click', () => {
+            let itemClick = JSON.parse(localStorage.getItem('allMebel'));
+            let name = itemClick.products[idx].name;
+            let img = itemClick.products[idx].img;
+            let price = itemClick.products[idx].price;
+            console.log(card_modal.childNodes[3].childNodes);
+
+            card_modal.classList.remove('card_modal__hidden');
+
+            card_modal.childNodes[1].childNodes[1].src = img;
+            card_modal.childNodes[3].childNodes[1].innerText = name;
+            card_modal.childNodes[3].childNodes[9].innerText = price + ' руб.';
+
+            let myModalEl = document.getElementById('myModal');
+            console.log(myModalEl);
+
+        });
     });
-   });
 
-   
+
 }
-
